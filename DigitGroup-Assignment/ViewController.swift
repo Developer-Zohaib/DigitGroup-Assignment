43761b9278f63ed3c375e9b436bba0ac7bc57f05
Zohaib Afzal <Zohaib.Afzal@osn.com>
2025-12-24 16:49:31 +0500

    Initial Commit

---
+++ DigitGroup-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigitGroup-Assignment
+//
+//  Created by zohaib afzal on 24/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
